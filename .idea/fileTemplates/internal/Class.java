4b343df6acd25ab22c643ad169b8ef4c5f267735
KotZava <truongnq2k@gmail.com>
2021-07-09 18:23:31 +0700

    🥝 Các loại biến trong Java

---
+++ .idea/fileTemplates/internal/Class.java
@@ -0,0 +1,13 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+/**
+ * Copyright 2021 {@author Quang Truong} (https://kotzava.com).
+ * This project is licensed under the MIT license.
+ *
+ * @since 08/07/2021
+ * Github: https://github.com/kotzava
+ */
+ 
+#parse("File Header.java")
+public class ${NAME} {
+}

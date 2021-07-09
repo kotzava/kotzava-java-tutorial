1c1976797dd118e749a5dbb1e6c099a9cf935231
KotZava <truongnq2k@gmail.com>
2021-07-09 18:37:53 +0700

    🍔 Edit header copyright

--- .idea/fileTemplates/internal/Class.java
+++ .idea/fileTemplates/internal/Class.java
@@ -4,7 +4,7 @@
  * Copyright 2021 {@author Quang Truong} (https://kotzava.com).
  * This project is licensed under the MIT license.
  *
- * @since 08/07/2021
+ * @since ${DATE}
  * Github: https://github.com/kotzava
  */
  

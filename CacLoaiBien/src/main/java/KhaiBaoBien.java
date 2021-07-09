d60c7e08cff940b1c720427b9dd84a3b277455d8
KotZava <truongnq2k@gmail.com>
2021-07-09 18:31:04 +0700

    🎾 Khai báo biến và đổi giá trị biến

---
+++ CacLoaiBien/src/main/java/KhaiBaoBien.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2021 {@author Quang Truong} (https://kotzava.com).
+ * This project is licensed under the MIT license.
+ *
+ * @since 08/07/2021
+ * Github: https://github.com/kotzava
+ */
+// 🍔 Khai báo biến trong Java
+public class KhaiBaoBien {
+    public static void main(String[] args) {
+        int soLuong = 15;
+        // hiển thị in ra màn hình biến soLuong khi khởi tạo
+        System.out.println(soLuong);
+        soLuong = 20;
+        // hiển thị ra màn hình biến soLuong khi đổi giá trị
+        System.out.println(soLuong);
+    }
+}

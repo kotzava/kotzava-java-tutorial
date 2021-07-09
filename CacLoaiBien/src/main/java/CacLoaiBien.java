41c324d16d17f26760ea6262586a77235e5b92c7
KotZava <truongnq2k@gmail.com>
2021-07-09 18:35:56 +0700

    🥊 Các loại biến trong Java

---
+++ CacLoaiBien/src/main/java/CacLoaiBien.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2021 {@author Quang Truong} (https://kotzava.com).
+ * This project is licensed under the MIT license.
+ *
+ * @since 09/07/2021
+ * Github: https://github.com/kotzava
+ */
+
+public class CacLoaiBien {
+
+    public static int soLuong = 15; // biến static
+
+    public void soSanPham(){
+        int sanPham = 15; // biến local
+    }
+
+    public static void main(String[] args) {
+        int duLieu = 15; // biến instance
+    }
+}
